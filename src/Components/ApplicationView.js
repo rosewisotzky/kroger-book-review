369d0b3d6b2e64e48908830df5fe5a02e8f85454
Rose Wisotzky <rose@rosewisotzky.com>
2019-05-01 10:54:24 -0500

    post works and redirects to book list, books are displayed on the DOM and posted to the database

--- src/Components/ApplicationView.js
+++ src/Components/ApplicationView.js
@@ -16,37 +16,41 @@ export default class ApplicationView extends Component {
     "genres": [],
     "userId": ""
   }
-
-  componentDidMount () {
+// componentDidMount runs after the render and calls our getAll fetch calls and sets the new state.
+  componentDidMount() {
     const newState = {}
     BookManager.getAll()
-    .then(books => newState.books=books)
-    .then(()=> this.setState(newState))
+      .then(books => newState.books = books)
+      .then(() => this.setState(newState))
     GenreManager.getAll()
-    .then(genre => newState.genres=genre)
-    .then(()=> this.setState(newState))
+      .then(genre => newState.genres = genre)
+      .then(() => this.setState(newState))
     KrogerManager.getAll()
-    .then(kroger => newState.krogers=kroger)
-    .then(()=> this.setState(newState))
+      .then(kroger => newState.krogers = kroger)
+      .then(() => this.setState(newState))
   }
+  // Here's our method addBook which takes that new book and using our POST call written our BookManager module sets the new state to have that book in it.
   addBook = newBook => {
-   return BookManager.post(newBook)
-    .then(books=> this.setState({"books": books}))
+    return BookManager.post(newBook)
+    .then(() => BookManager.getAll())
+      .then(books => this.setState({ "books": books }))
+
   }
 
-  render () {
+  render() {
     return (
-    <React.Fragment>
-      <Route path="/" render={props => {
-        return  null } 
-      } />
-      <Route exact path="/booklist" render={props => {
-        return <BookList books={this.state.books} />
-      }} />
-      <Route path="/addbook" render={props => {
-        return <AddBookForm {...props} genres={this.state.genres} krogers={this.state.krogers} addBook={this.addBook}/>
-      }} />
-    </React.Fragment>
+      <React.Fragment>
+        <Route path="/" render={props => {
+          return null
+        }
+        } />
+        <Route exact path="/booklist" render={props => {
+          return <BookList books={this.state.books} {...props}/>
+        }} />
+        <Route path="/addbook" render={props => {
+          return <AddBookForm {...props} books={this.state.books} genres={this.state.genres} krogers={this.state.krogers} addBook={this.addBook} />
+        }} />
+      </React.Fragment>
     )
   }
 
