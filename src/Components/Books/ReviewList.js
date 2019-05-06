cde40c792e81bf4e4b44bd3264b994bc2e36fa2f
Rose Wisotzky <rose@rosewisotzky.com>
2019-05-06 13:10:38 -0500

    only the users books that have not been reviewed appear on review

--- src/Components/Books/ReviewList.js
+++ src/Components/Books/ReviewList.js
@@ -4,6 +4,10 @@ import { Link } from "react-router-dom"
 export default class ReviewList extends Component {
 
     // In this component we are rendering the JSX to create our list of books. 
+    state = {
+        currentUser: sessionStorage.getItem("userID")
+    }
+    
     render() {
         // console.log("books", this.props.books)
         return (
@@ -14,7 +18,7 @@ export default class ReviewList extends Component {
                     // We are mapping through the array of books in our database and plopping them into a new array. For each book, we are building the JSX to list the title, author, genre, and location. 
                     this.props.books.map(book => {
                         // If the book has an empty string as the value of the key review, we will not list the review.
-                        if (book.review === "")
+                        if (book.review === "" && parseInt(book.userId) === parseInt(this.state.currentUser))
                             return <div key={book.id}>
                                 <p>
                                     <Link className="nav-link" to={`/review/${book.id}/addreview`}>{book.title}</Link>  <br />
